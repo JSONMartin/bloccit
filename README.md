dd75b7d3285d619cf3ad5f79df3c8e9dd2654a0b
Jason Martin <jason.carter.martin@gmail.com>
2013-12-23 16:50:31 -0800

    First commit and README update

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bloccit
+
+A Reddit-style WebApp to demonstrate foundation programming and web development skills.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = 'e274d858e725b6e9547d0c6b208febd2ba6eec2418b292f59c65b25fb268aee2d9e8cd4165abd521baf4a34931518c6169153cca193791e0f0682a5ad67acddd'
