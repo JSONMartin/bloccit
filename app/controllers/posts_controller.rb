1ecfb389cd683d9b0b7ad0d79d0bca8c28e32d79
Jason Martin <jason.carter.martin@gmail.com>
2013-12-31 17:00:07 -0800

    Associated and nested topics and posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,28 @@
 class PostsController < ApplicationController
+
+## Depricated Code
+=begin  
   def index
     @posts = Post.all
   end
+=end
 
   def show
     @post = Post.find(params[:id])
+    @topic = Post.find(params[:topic_id])
   end
 
   def new
     @post = Post.new
+    @topic = Post.find(params[:topic_id])
     authorize! :create, Post, message: "You need to be a member to create a new post."
   end
 
   def create
-    #@post = Post.new(params[:post]) ##OLD CODE
+    @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(params[:post])
+    @post.topic = @topic
+
     authorize! :create, @post, message: "You need to be signed up to do that."
     if @post.save
       flash[:notice] = "Post was saved."
@@ -26,16 +34,18 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize! :edit, @post, message: "You need to be authorized to do that."
   end
 
   def update
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize! :update, @post, message: "You need to be authorized to do that."
     if @post.update_attributes(params[:post])
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :edit
