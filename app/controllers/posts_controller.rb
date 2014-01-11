17dbf698d1f6385ff536858eb16fccbbc0e0cf0e
Jason Martin <jason.carter.martin@gmail.com>
2014-01-10 17:55:45 -0800

    Scoped topics for privacy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @topic = Topic.find(params[:topic_id])
+    authorize! :read, @topic, message: "You need to be signed-in to do that."
     @comments = @post.comments
   end
 
